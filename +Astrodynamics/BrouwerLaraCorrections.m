2cae7c923da3ce9a6899754deefe67db1b7b3aae
SergioCdV <66524268+SergioCdV@users.noreply.github.com>
2024-03-20 21:19:42 +0100

    <update>: dimesional and vectorized BrouwerLara

--- +Astrodynamics/BrouwerLaraCorrections.m
+++ +Astrodynamics/BrouwerLaraCorrections.m
@@ -15,10 +15,10 @@
 
 % Ouput: - S, an array with the same dimensions of L, containing the transformed Lara's variables
 
-function [S] = BrouwerLaraCorrections(J2, J3, H, L, direction)
+function [S] = BrouwerLaraCorrections(Re, J2, J3, H, L, direction)
     % Perturbations parameters in the expansion of the Hamiltonian
-    epsilon(1) = -J2/4;              % 1/4 J2 * 1       (non-dimensional Re)
-    epsilon(2) = +(J3/J2) / 2;       % 1/2 (J3/J2) * 1  (non-dimensional Re)
+    epsilon(1) = -J2/4 * Re^2;            % - 1/4 J2 * Re^2     (direct change coefficient)
+    epsilon(2) = +(J3/J2) * Re / 2;       % 1/2 (J3/J2) * Re  (direct change coefficient)
 
     if (~exist('direction', 'var'))
         direction = true;
@@ -26,6 +26,7 @@ function [S] = BrouwerLaraCorrections(J2, J3, H, L, direction)
 
     if (~direction)
         epsilon = -1 * epsilon;
+        
         % Osculating to long-period transformation (short period transformation)
         L_long = Long2Osc(H, epsilon(1), L); 
 
@@ -60,12 +61,12 @@ function [Do] = Mean2Long(epsilon, L)
     c = sqrt(1-chi.^2-xi.^2);
 
     % Transformation
-    psi_l = psi + eps * (2*xi + (k .* xi - c .* chi .* sigma) ./ (1+c));
-    chi_l = chi + eps * (2*xi.^2 + k .* (1-chi.^2));
-    xi_l = xi - eps * (c.^2 .* sigma + (2+k) .* chi .* xi);                                              
-    r_l = r + eps * chi .* p;
-    R_l = R + eps * (1+k) .* xi .* Theta ./ r;
-    Theta_l = Theta + eps * (k .* chi - sigma .* xi) .* Theta;
+    psi_l = psi + eps .* (2*xi + (k .* xi - c .* chi .* sigma) ./ (1+c));
+    chi_l = chi + eps .* (2*xi.^2 + k .* (1-chi.^2));
+    xi_l = xi - eps .* (c.^2 .* sigma + (2+k) .* chi .* xi);                                              
+    r_l = r + eps .* chi .* p;
+    R_l = R + eps .* (1+k) .* xi .* Theta ./ r;
+    Theta_l = Theta + eps .* (k .* chi - sigma .* xi) .* Theta;
 
     % Output
     Do = [psi_l; chi_l; xi_l; r_l; R_l; Theta_l];
@@ -85,12 +86,12 @@ function [Lo] = Long2Osc(H, epsilon, L)
     epsilon = epsilon./p.^2;     % J2/p^2
 
     % Transformation
-    psi_o = psi - epsilon * (1+7*c)./(1+c) .* chi .* xi;
-    chi_o = chi - epsilon * (xi.^2-3*c.^2) .* chi;
-    xi_o = xi + epsilon * (chi.^2-3*c.^2) .* xi;
-    r_o = r + 2 * epsilon * r .* (chi.^2-2+5*c.^2);                                                      
-    R_o = R + 4 * epsilon * Theta./r .* chi .* xi;
-    Theta_o = Theta + 3 * epsilon * Theta .* (chi.^2-xi.^2);                                                 
+    psi_o = psi - epsilon .* (1+7*c)./(1+c) .* chi .* xi;
+    chi_o = chi - epsilon .* (xi.^2-3*c.^2) .* chi;
+    xi_o = xi + epsilon .* (chi.^2-3*c.^2) .* xi;
+    r_o = r + 2 * epsilon .* r .* (chi.^2-2+5*c.^2);                                                      
+    R_o = R + 4 * epsilon .* Theta./r .* chi .* xi;
+    Theta_o = Theta + 3 * epsilon .* Theta .* (chi.^2-xi.^2);                                                 
 
     % Output
     Lo = [psi_o; chi_o; xi_o; r_o; R_o; Theta_o];
--- +Test/UKF_Milankovitch.m
+++ +Test/UKF_Milankovitch.m
@@ -1,5 +1,4 @@
 %% Orbital estimation Example %%
-% Author: Sergio Cuevas 
 % Date: 22/01/2024
 % This file provides an UKF filter implementation for the Milankovitch dynamics
 
@@ -8,16 +7,19 @@ close all;
 clc;
 rng(1); 
 format long; 
-set_graphics(); 
+
+set_graphics();
 
 %% Initial conditions and set up 
 % Canonical units are assumed for the sake of simplicity
+Re = 1;                 % Radius of the Earth (in Earth radii)
 J2 = 1.08263e-3;        % J2 parameter of the Earth
+J3 = J2^2;                  % J3 parameter of the Earth
 Keci = [0;0;1];         % Third unit vector in the ECI reference frame triad
 
-% Milankovitch initial conditions in the ECI frame
-h0 = [0; 0; 1.8];         % Initial angular momentum vector (equatorial orbit) [-]
-e0 = [0.001; 0; 0];     % Initial eccentricity vector (equatorial orbit of 0.001 eccentricity, quasi-circular) [-]
+% Milankovitch initial conditions in the ECI frame (non-dimensional units)
+h0 = [0; 0; 1.8];       % Initial angular momentum vector (equatorial orbit) [-]
+e0 = [0.1; 0; 0];     % Initial eccentricity vector (equatorial orbit of 0.001 eccentricity, quasi-circular) [-]
 l0 = 0;                 % Initial longitude [rad]
 s0 = [h0; e0; l0];      % Complete initial state
 
@@ -27,27 +29,49 @@ options = odeset('AbsTol', 1E-22, 'RelTol', 2.25E-14);      % Integration tolera
 % Dynamics time span 
 N = 1E3;                        % Number of steps
 t0 = 0;                         % Initial epoch
-tf = 20;                         % Final epoch [orbital periods at the Earth's WGS84 radius]
+tf = 20;                        % Final epoch [orbital periods at the Earth's WGS84 radius]
 tspan = linspace(t0, tf, N);    % Integration time span
 
+
 %% Propagation 
-[tp, s] = ode45(@(t,s)Astrodynamics.milankovitch_dynamics(1, J2, Keci, t, s), tspan, s0, options);
+[tp, s] = ode45(@(t,s)utils.milankovitch_dynamics(Re, J2, Keci, t, s), tspan, s0, options);
+
+% Reduced MKV state 
+s = s(:,[1:5 7]);       % Disregard the inertial z component of the eccentricity vector
+
+%% Lara's transformation test 
+% Determine the third component of the eccentricity to handle a 6 x 1 state vector
+e = -dot(s(1,1:2), s(1,4:5), 1) ./ s(1,3);
+st = [s(1,1:5) e s(1,6)].';
+
+% Transform to osculating position and velocity 
+mkv_rv = utils.MKV2ECI(1, st, true);                                        % From MKV to Cartesian
+LM = utils.Lara2ECI(st(3,1), mkv_rv, false);                                % From Cartesian to Lara
+LEO = utils.BrouwerLaraCorrections(1, J2, J3, st(3,1), LM);                 % Mean to oculating Lara
+rv = utils.Lara2ECI(st(3,1), LEO, true);                                    % Osculating Cartesian
+LEOb = utils.Lara2ECI(st(3,1), rv, false);                                  % Osculating Lara
+
+test(1) = norm(LEO-LEOb);
+
+LMb = utils.BrouwerLaraCorrections(1, J2, J3, st(3,1), LEOb, 0);            % Mean Lara
+test(2) = norm(LM-LMb);
 
 %% Offline observation 
 % Constants 
-Pobs = blkdiag( 1e-1 * eye(3), 1e-9 * eye(3), 0.1 );             % Covariance of the observation 
+Pobs = blkdiag( 1e-9 * eye(3), 1e-9 * eye(3), 1E-2 );             % Covariance of the observation 
 
 % Assume a linear identity observation model (H = I)
-PD = 0.9;                                                        % Probability of observation
+PD = 0.3;                                                        % Probability of observation
 index = logical(randsrc(size(s,1), 1, [0, 1; 1-PD, PD]));        % Observation epochs
 
 % Observed state vector
 sobs = [tp(index) s(index,:)];
 
-meas = sobs;
+meas = zeros(size(sobs,1), 8);                              % Preallocation of the measurements
+meas(:,1) = tp(index);
 for i = 1:size(sobs,1)
-    meas(i,2:end) = observation_model(sobs(i,2:end));   % Go through the measurement model
-    meas(i,2:end) = mvnrnd(sobs(i,2:end), Pobs, 1);     % Add some noises
+    meas(i,2:end) = observation_model(sobs(i,2:end).').';   % Go through the measurement model
+    meas(i,2:end) = meas(i,2:end) + mvnrnd(zeros(size(meas(i,2:end))), Pobs, 1);         % Add some noises
 end
 
 %% Offline estimation (EKF)
@@ -56,28 +80,30 @@ fprintf('------------------------------\n');
 fprintf('Running the filter: \n');
 
 % Constants 
-Q = 1e-5 * eye(7);      % Dynamical noise 
-R = 1e-8 * eye(7);      % Measurement noise 
+Q = 1e-5 * eye(6);      % Dynamical noise 
+R = 1e-3 * eye(7);      % Measurement noise 
 
-s_dim = 7;              % Dimension of the state vector
+s_dim = 6;              % Dimension of the (reduced) state vector
 y_dim = 7;              % Dimension of the observation vector
 
 % Estimator setup
-beta = 2; 
-alpha = 1E-3; 
-k = 0;
-UKF_estimator = Filters.UKF('UKF-S', beta, alpha, k);
+beta = 2;               % Spread of the sigma points
+alpha = 1E-3;           % Spread of the sigma points
+k = 0;                  % Spread of the sigma points
+
+UKF_estimator = aux_filters.UKF('UKF-S', beta, alpha, k);
+UKF_estimator = UKF_estimator.AssignStateProcess(s_dim, @(s, time_step)state_model(1, J2, Keci, s, time_step));
 UKF_estimator = UKF_estimator.AdditiveCovariances(Q, R);
-UKF_estimator = UKF_estimator.AssignStateProcess(s_dim, @(s, time_step)state_model(J2, Keci, s, time_step)).Init();
 UKF_estimator = UKF_estimator.AssignObservationProcess(y_dim, @(s)observation_model(s));
+UKF_estimator = UKF_estimator.Init();
 
 % Initial conditions 
-se = [0; 0; 1.5; 0; 0; 0.01; 0];        % Estimator initial conditions 
-Pe = 1e-8 * eye(7);                     % Initial covariance matrix
+se = [0; 0; 1.5; 0; 0; 0];        % Estimator initial conditions 
+Pe = 1e-3 * eye(6);               % Initial covariance matrix
 
 % Main loop
 i = 1;
-epoch = tp(1);                           % Initial epoch
+epoch = tp(1);                    % Initial epoch
 
 while (i <= size(meas,1))
     % Propagation 
@@ -116,15 +142,15 @@ e = sobs(:,2:end).' - S;
 
 % Compute the 3sigma bounds and rotate the output reference frame to diagonalize P
 sigma = zeros(s_dim, length(t));
-for i = 1:length(t)
-    Sigma = reshape(P(:,i), s_dim, s_dim);
-    [V, lambda] = eig(Sigma);
-    
-    sigma(:,i) = sqrt( diag(lambda) );
-    e(:,i) = V * e(:,i);
-end
+% for i = 1:length(t)
+%     Sigma = reshape(P(:,i), s_dim, s_dim);
+%     [V, lambda] = eig(Sigma);
+%     
+%     sigma(:,i) = real( sqrt( diag(lambda) ) );
+%     e(:,i) = V \ e(:,i);
+% end
 
-e = e.';
+error = e.';
 sigma = sigma.';
 
 %% Plot and results
@@ -141,9 +167,10 @@ grid on
 ylabel('$\|\mathbf{h}\|$ [-]')
 legend('$h_x$', '$h_y$', '$h_z$')
 
+e = -dot(s(:,1:2), s(:,4:5), 2) ./ s(:,3);
 subplot(3,1,2)
 hold on
-plot(tp, s(:,4:6))
+plot(tp, [s(:,4:5) e])
 scatter(meas(:,1), meas(:,5), 'Marker', 'x')
 scatter(meas(:,1), meas(:,6), 'Marker', 'x')
 scatter(meas(:,1), meas(:,7), 'Marker', 'x')
@@ -154,7 +181,7 @@ legend('$e_x$', '$e_y$', '$e_z$')
 
 subplot(3,1,3)
 hold on
-plot(tp, s(:,7), 'b')
+plot(tp, s(:,6), 'b')
 scatter(meas(:,1), meas(:,8), 'r', 'Marker', 'x')
 hold off
 ylabel('$l$ [rad]')
@@ -169,7 +196,7 @@ plot(tp, s(:,3), 'b')
 ylabel('$h_z$ [-]')
 grid on
 subplot(1,2,2)
-plot(tp, sqrt( dot(s(:,4:6), s(:,4:6), 2) ), 'b')
+plot(tp, sqrt( dot([s(:,4:5) e], [s(:,4:5) e], 2) ), 'b')
 ylabel('$\|\mathbf{e}\|$ [-]')
 grid on
 xlabel('$t$')
@@ -178,53 +205,53 @@ xlabel('$t$')
 figure 
 subplot(3,1,1)
 hold on
-plot(t, e(:,1), 'b')
-plot(t, e(:,1) + 3 * sigma(:,1))
-plot(t, e(:,1) - 3 * sigma(:,1))
+plot(t, error(:,1), 'b')
+plot(t, error(:,1) + 3 * sigma(:,1))
+plot(t, error(:,1) - 3 * sigma(:,1))
 grid on
 ylabel('$\Delta h_x$ [-]')
 subplot(3,1,2)
 hold on
-plot(t, e(:,2), 'b')
-plot(t, e(:,2) + 3 * sigma(:,2))
-plot(t, e(:,2) - 3 * sigma(:,2))
+plot(t, error(:,2), 'b')
+plot(t, error(:,2) + 3 * sigma(:,2))
+plot(t, error(:,2) - 3 * sigma(:,2))
 grid on
 ylabel('$\Delta h_y$ [-]')
 subplot(3,1,3)
 hold on
-plot(t, e(:,3), 'b')
-plot(t, e(:,3) + 3 * sigma(:,1))
-plot(t, e(:,3) - 3 * sigma(:,1))
+plot(t, error(:,3), 'b')
+plot(t, error(:,3) + 3 * sigma(:,1))
+plot(t, error(:,3) - 3 * sigma(:,1))
 grid on
 ylabel('$\Delta h_z$ [-]')
 xlabel('$t$')
 
 figure 
-subplot(3,1,1)
-plot(t, e(:,4), 'b')
-plot(t, e(:,4) + 3 * sigma(:,4))
-plot(t, e(:,4) - 3 * sigma(:,4))
+subplot(2,1,1)
+plot(t, error(:,4), 'b')
+plot(t, error(:,4) + 3 * sigma(:,4))
+plot(t, error(:,4) - 3 * sigma(:,4))
 grid on
 ylabel('$\Delta e_x$ [-]')
-subplot(3,1,2)
-plot(t, e(:,5), 'b')
-plot(t, e(:,5) + 3 * sigma(:,5))
-plot(t, e(:,5) - 3 * sigma(:,5))
+subplot(2,1,2)
+plot(t, error(:,5), 'b')
+plot(t, error(:,5) + 3 * sigma(:,5))
+plot(t, error(:,5) - 3 * sigma(:,5))
 grid on
 ylabel('$\Delta e_y$ [-]')
-subplot(3,1,3)
-plot(t, e(:,6), 'b')
-plot(t, e(:,6) + 3 * sigma(:,6))
-plot(t, e(:,6) - 3 * sigma(:,6))
-grid on
-ylabel('$\Delta e_z$ [-]')
-xlabel('$t$')
+% subplot(3,1,3)
+% plot(t, error(:,6), 'b')
+% plot(t, error(:,6) + 3 * sigma(:,6))
+% plot(t, error(:,6) - 3 * sigma(:,6))
+% grid on
+% ylabel('$\Delta e_z$ [-]')
+% xlabel('$t$')
 
 figure 
 hold on
-plot(t, e(:,7), 'b')
-plot(t, e(:,7) + 3 * sigma(:,7))
-plot(t, e(:,7) - 3 * sigma(:,7))
+plot(t, error(:,6), 'b')
+plot(t, error(:,6) + 3 * sigma(:,6))
+plot(t, error(:,6) - 3 * sigma(:,6))
 grid on
 ylabel('$\Delta l$ [rad]')
 xlabel('$t$')
@@ -238,25 +265,56 @@ xlabel('$t$')
 
 %% Auxiliary functions 
 % Propagation model 
-function [s] = state_model(J2, Keci, s0, dt)
+function [s] = state_model(Re, J2, Keci, s0, dt)
     % Integration of the state vector 
     options = odeset('AbsTol', 1E-22, 'RelTol', 2.25E-14);      % Integration tolerances
-    
+
+    % Determine the third component of the eccentricity to handle a 6 x 1 state vector
+    e = -dot(s0(1:2,:), s0(4:5,:), 1) ./ s0(3,:);
+    s0 = [s0(1:5,:); e; s0(6,:)];
+
+    % Integrate the whole model
     if (dt > 0)
         S0 = reshape(s0, [], 1);
-        [~, saux] = ode45(@(t,s)Astrodynamics.milankovitch_dynamics(1, J2, Keci, t, s), [0 dt], S0, options);
+        [~, saux] = ode45(@(t,s)utils.milankovitch_dynamics(Re, J2, Keci, t, s), [0 dt], S0, options);
         s = saux(end,:); 
         s = reshape(s, size(s0));
     else
         s = s0;
     end
+
+    % Eliminate the third component of the eccentricity to handle a 6 x 1 state vector
+    s = s([1:5 7],:);
 end
 
 % Observation model 
 function [y, H] = observation_model(s)
+    % Determine the third component of the eccentricity to handle a 6 x 1 state vector
+    e = -dot(s(1:2,:), s(4:5,:), 1) ./ s(3,:);
+    s = [s(1:5,:); e; s(6,:)];
+    
     % Linear identity model
     y = s;                                   % Measurements
     H = eye(size(s,1), size(s,1));           % Observation matrix
+end
+
+% RADAR observation model
+function [y, H] = radar_model(structData, PARAMS, init_epoch, t, s)
+    % Determine the third component of the eccentricity to handle a 6 x 1 state vector
+    e = -dot(s(1:2,:), s(4:5,:), 1) ./ s(3,:);
+    s = [s(1:5,:); e; s(6,:)];
+
+    % Transform to osculating position and velocity 
+    mkv_rv = utils.MKV2ECI(1, s, true);                                                 % From MKV to Cartesian
+    LM = utils.Lara2ECI(s(3,1), mkv_rv, false);                                         % From Cartesian to Lara
+    LEO = utils.BrouwerLaraCorrections(1, tle.rec.j2, tle.rec.j3, s0(3,1), LM);         % Mean to oculating Lara
+    rv = utils.Lara2ECI(s(3,1), LEO, true);                                             % Osculating Cartesian  
+
+    % Apply the radar model 
+    dateObs = init_epoch + t;
+    y = observationRadar(structData.xStateatEpoch', dateObs, rv(1:3,1)*1e-3, PARAMS);  % Measurements
+
+    H = [];           % Observation matrix (not neede for UKF)
 end
 
 function set_graphics()
